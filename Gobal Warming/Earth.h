d6ee6a48f5af54243dd1320241ea917ff0eef1ba
Hitesh Sathawane <hiteshsathawane@live.com>
2017-11-11 01:00:16 +0530

    Clean Code

---
+++ Gobal Warming/Earth.h
@@ -0,0 +1,14 @@
+#pragma once
+// Header File of Global Warming Project
+
+class Earth {
+public:
+
+void InitializeSimulation();
+void UpdateSimulation();
+void DrawSimulation();
+void LoadingSimulation();
+void MainMenu();
+
+
+};
